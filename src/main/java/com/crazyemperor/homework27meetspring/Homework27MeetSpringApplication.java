95ae68cb0c8bb61dea561ba797d6c72809aebf68
Andrey <stsiatskou@gmail.com>
2023-04-24 20:13:53 +0200

    Initial commit

---
+++ src/main/java/com/crazyemperor/homework27meetspring/Homework27MeetSpringApplication.java
@@ -0,0 +1,13 @@
+package com.crazyemperor.homework27meetspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework27MeetSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework27MeetSpringApplication.class, args);
+    }
+
+}
